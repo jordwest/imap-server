7cc006b42e34074b2094318737f5b58b1ae0694a
Jordan West <jordwest@gmail.com>
2015-05-09 15:17:15 +0900

    Use correct casing for (and de-export) constant #8

--- command_list.go
+++ command_list.go
@@ -1,12 +1,12 @@
 package imap
 
-const LIST_ARG_SELECTOR int = 1
+const listArgSelector int = 1
 
 func cmdList(args commandArgs, c *Conn) {
-	if args.Arg(LIST_ARG_SELECTOR) == "" {
+	if args.Arg(listArgSelector) == "" {
 		// Blank selector means request directory separator
 		c.writeResponse("", "LIST (\\Noselect) \"/\" \"\"")
-	} else if args.Arg(LIST_ARG_SELECTOR) == "*" {
+	} else if args.Arg(listArgSelector) == "*" {
 		// List all mailboxes requested
 		for _, mailbox := range c.user.Mailboxes() {
 			c.writeResponse("", "LIST () \"/\" \""+mailbox.Name()+"\"")
