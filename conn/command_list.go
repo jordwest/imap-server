ed12dfae544c8b170f4600d461460ecdc968915a
Jordan West <jordwest@gmail.com>
2015-07-13 10:34:41 +0900

    Ensure user is logged in before LIST command

--- conn/command_list.go
+++ conn/command_list.go
@@ -3,6 +3,10 @@ package conn
 const listArgSelector int = 1
 
 func cmdList(args commandArgs, c *Conn) {
+	if !c.assertAuthenticated(args.ID()) {
+		return
+	}
+
 	if args.Arg(listArgSelector) == "" {
 		// Blank selector means request directory separator
 		c.writeResponse("", "LIST (\\Noselect) \"/\" \"\"")
