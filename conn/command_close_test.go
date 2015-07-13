f60b209d4cd58439650ea9b7f8e293998faa8227
Jordan West <jordwest@gmail.com>
2015-07-13 11:07:29 +0900

    Added test stubs for remaining commands #14

---
+++ conn/command_close_test.go
@@ -0,0 +1,27 @@
+package conn_test
+
+import (
+	"github.com/jordwest/imap-server/conn"
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("CLOSE Command", func() {
+	Context("When logged in", func() {
+		BeforeEach(func() {
+			tConn.SetState(conn.StateAuthenticated)
+			tConn.User = mStore.User
+		})
+
+		PIt("should (implement test)", func() {
+		})
+	})
+
+	Context("When not logged in", func() {
+		BeforeEach(func() {
+			tConn.SetState(conn.StateNotAuthenticated)
+		})
+
+		PIt("should give an error", func() {
+		})
+	})
+})
